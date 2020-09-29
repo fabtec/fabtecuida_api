8aa93b5dfb1062a2f4ccc2aa9631aedf1232adab
= <=>
2020-09-29 03:28:57 -0300

    change engine db production to postgis

--- fabtecuida_api/settings.py
+++ fabtecuida_api/settings.py
@@ -117,6 +117,7 @@ if os.environ.get('DATABASE_URL', ''):
     DATABASES = {
         'default': dj_database_url.config()
     }
+    DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 else:
     # local env
     DATABASES = {
